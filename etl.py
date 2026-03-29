8c58eebec753a4b66d6999f038f9790dbd8212ba
Omar Nasr Mohamed <omarnasr628@gmail.com>
2026-03-29 22:24:31 +0200

    C1
    Removed unnecessary docstrings and comments in etl.py.

--- etl.py
+++ etl.py
@@ -4,32 +4,19 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
-    """loads the data in the staging tables"""
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
-    """insert the data in the tables"""
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    """
-    - read the config file . 
-    
-    - Establishes connection with the database and gets
-     cursor to it.  
-    
-    - loads the data in the staging tables.  
-    
-    - insert the data in the tables. 
-    
-    - Finally, closes the connection. 
-    """
+   
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
